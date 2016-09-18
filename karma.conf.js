fb471d0a26e837d7ddb489d291209f79df602c14
Jonathan Soler <jonathan.soler@viseo.com>
2016-09-18 16:27:50 +0200

    add karma-jasmine-html-reporter plugin

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,8 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
-      require('angular-cli/plugins/karma')
+      require('angular-cli/plugins/karma'),
+      require('karma-jasmine-html-reporter')
     ],
     files: [
       { pattern: './src/test.ts', watched: false }
@@ -27,7 +28,22 @@ module.exports = function (config) {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters: ['progress', 'karma-remap-istanbul'],
+    reporters: ['progress', 'karma-remap-istanbul', 'kjhtml'],
+    // the default configuration
+    htmlReporter: {
+      outputDir: 'karma_html', // where to put the reports
+      templatePath: null, // set if you moved jasmine_template.html
+      focusOnFailures: true, // reports show failures on start
+      namedFiles: false, // name files instead of creating sub-directories
+      pageTitle: null, // page title for reports; browser info by default
+      urlFriendlyName: false, // simply replaces spaces with _ for files/dirs
+      reportName: 'report-summary-filename', // report summary filename; browser info by default
+
+
+      // experimental
+      preserveDescribeNesting: false, // folded suites stay folded
+      foldAll: false, // reports start folded (only with preserveDescribeNesting)
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
