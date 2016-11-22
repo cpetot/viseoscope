beb3bc9e39ec245946a522f70fbd793cd516f265
Claude Petot <claude.petot@gmail.com>
2016-11-22 16:11:30 +0100

    Utilisation correcte de @Ouput
    - @Output était utilisé de manière non recommandée par la doc Angular
    - @Output est plutôt à utiliser pour lancer des évènements et pas pour
      en même temps mettre à jour une variable d'un autre composant.
    - Refactoring en conséquence.
    - Mise à jour du .gitignore

--- src/app/collaborator-detail/collaborator-detail.component.spec.ts
+++ src/app/collaborator-detail/collaborator-detail.component.spec.ts
@@ -1,40 +1,34 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { CollaboratorDetailComponent } from './collaborator-detail.component';
-import { CollaboratorService } from '../collaborator.service';
-import { Collaborator } from '../collaborator';
+import {TestBed, inject} from "@angular/core/testing";
+import {CollaboratorDetailComponent} from "./collaborator-detail.component";
+import {Collaborator} from "../collaborator";
 
 describe('Component: CollaboratorDetail', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CollaboratorService]
     });
   });
 
-  it('should create an instance',
-  inject([CollaboratorService],(service:CollaboratorService) => {
-    let component = new CollaboratorDetailComponent(service);
+    it('should create an instance', inject([], () => {
+        let component = new CollaboratorDetailComponent();
     expect(component).toBeTruthy();
   }));
 
-  it('should delegate creation to the service when onCreate',
-  inject([CollaboratorService],(service:CollaboratorService) => {
+    it('should delegate creation to the service when onCreate', inject([], () => {
 
     // Given
-    let component = new CollaboratorDetailComponent(service);
+        let component = new CollaboratorDetailComponent();
     component.creation = true;
 
     let collaborator = new Collaborator(1, 'foo', 'bar');
-    spyOn(service, 'addCollaborator');
-    spyOn(component.creationChange, 'emit');
+        spyOn(component.createRequest, 'emit');
 
     // When
     component.onCreate(collaborator);
-    expect(service.addCollaborator).toHaveBeenCalledWith(collaborator);
     expect(component.creation).toBe(false);
-    expect(component.creationChange.emit).toHaveBeenCalledWith(false);
+        expect(component.createRequest.emit).toHaveBeenCalledWith(collaborator);
 
   }));
 });
--- src/app/collaborator-detail/collaborator-detail.component.ts
+++ src/app/collaborator-detail/collaborator-detail.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Collaborator } from '../collaborator';
-import { CollaboratorService } from '../collaborator.service'
+import {Component, OnInit, Input, Output, EventEmitter} from "@angular/core";
+import {Collaborator} from "../collaborator";
 
 @Component({
   selector: 'viseo-collaborator-detail',
@@ -11,17 +10,17 @@ export class CollaboratorDetailComponent implements OnInit {
   @Input() collaborator: Collaborator;
   @Input() creation: boolean = false;
   //utile pour le double-databing
-  @Output() creationChange:EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() createRequest: EventEmitter<Collaborator> = new EventEmitter<Collaborator>();
 
-  constructor(private _collaboratorService : CollaboratorService) {}
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
   onCreate(collaborator : Collaborator) {
-    this._collaboratorService.addCollaborator(collaborator);
     this.creation = false;
     //on informe le composant parent que l'on a fini de créer le collaborateur
-    this.creationChange.emit(this.creation);
+    this.createRequest.emit(collaborator);
   }
 }
