0d168b2e87850bf296460f979ca08043e192fbcb
Claude Petot <claude.petot@gmail.com>
2016-12-20 09:33:39 +0100

    Passage à Angular CLI beta 22 et Angular 2.2.3

--- src/app/collaborators/collaborators.component.ts
+++ src/app/collaborators/collaborators.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Collaborator } from '../collaborator';
+import {Component, OnInit} from "@angular/core";
+import {Collaborator} from "../collaborator";
 
 @Component({
   selector: 'viseo-collaborators',
-  templateUrl: 'collaborators.component.html',
-  styleUrls: ['collaborators.component.css']
+  templateUrl: './collaborators.component.html',
+  styleUrls: ['./collaborators.component.css']
 })
 export class CollaboratorsComponent implements OnInit {
-  collaborators : Array<Collaborator> = [
+  collaborators: Array<Collaborator> = [
     new Collaborator(1, 'Petot', 'Claude'),
     new Collaborator(2, 'Soler', 'Jonathan')
   ];
 
-  constructor() {}
+  constructor() {
+  }
 
   ngOnInit() {
   }
