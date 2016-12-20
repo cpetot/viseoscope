790f688a97ae0adcb3bfac3dcbe2ebd3c47dabbd
Claude Petot <claude.petot@gmail.com>
2016-12-20 09:33:39 +0100

    Passage à Angular CLI beta 22 et Angular 2.2.3
    Conflicts:
    	src/app/collaborators/collaborators.component.ts

--- src/app/collaborators/collaborators.component.ts
+++ src/app/collaborators/collaborators.component.ts
@@ -2,26 +2,26 @@ import {Component, OnInit} from "@angular/core";
 import {Collaborator} from "../collaborator";
 
 @Component({
-    selector: 'viseo-collaborators',
-    templateUrl: 'collaborators.component.html',
-    styleUrls: ['collaborators.component.css'],
+  selector: 'viseo-collaborators',
+  templateUrl: './collaborators.component.html',
+  styleUrls: ['./collaborators.component.css']
 })
 export class CollaboratorsComponent implements OnInit {
-    collaborators: Array<Collaborator> = [
-        new Collaborator(1, 'Petot', 'Claude'),
-        new Collaborator(2, 'Soler', 'Jonathan')
-    ];
-    selectedCollaborator: Collaborator;
+  collaborators: Array<Collaborator> = [
+    new Collaborator(1, 'Petot', 'Claude'),
+    new Collaborator(2, 'Soler', 'Jonathan')
+  ];
 
-    constructor() {
-    }
+  selectedCollaborator: Collaborator;
 
-    ngOnInit() {
+  constructor() {
+  }
 
-    }
+  ngOnInit() {
+  }
 
-    onCollaboratorSelected(collaborator: Collaborator) {
-        this.selectedCollaborator = collaborator;
-    }
+  onCollaboratorSelected(collaborator: Collaborator) {
+    this.selectedCollaborator = collaborator;
+  }
 
 }
